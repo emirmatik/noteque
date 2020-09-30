6cc93384635eb354120b8a126289a09870174022
Emir <emirmatik16@gmail.com>
2020-09-30 09:34:08 +0300

    Create LoadingPage.js

---
+++ src/pages/LoadingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Loading() {
+    return (
+        <div id="loading-page">
+            <div className="loading-baloon1 "></div>
+            <h3>Noteque</h3>
+            <div className="loading-baloon2"></div>
+        </div>
+    )
+}
+
+export default Loading;
